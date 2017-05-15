3c9634ae56ffd05bb78ae2ba8bbb3ee92cf8e061
ggalmazor <ggalmazor@gmail.com>
2017-05-15 17:18:27 +0200

    Tuneando informe de covertura

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,14 +1,34 @@
 def yarn = { -> "${tool 'yarn'}/bin/yarn" }
 
 pipeline {
-  agent any
+  agent {
+    label 'slave3'
+  }
   stages {
-    stage("build") {
+    stage("Dependencies") {
       steps {
         sh "${yarn()}"
-        sh "${yarn()} lint"
-        sh "${yarn()} test"
-        sh "${yarn()} coverage"
+      }
+    }
+    stage("Verifications") {
+      steps {
+        parallel lint: { sh "${yarn()} lint" },
+            test: { sh "${yarn()} test" },
+            coverage: {
+              sh "${yarn()} coverage"
+              step([$class             : 'CoberturaPublisher',
+                    autoUpdateHealth   : false,
+                    autoUpdateStability: false,
+                    coberturaReportFile: 'coverage/cobertura-coverage.xml',
+                    failUnstable       : false,
+                    maxNumberOfBuilds  : 0,
+                    zoomCoverageChart  : false
+              ])
+            }
+      }
+    }
+    stage("Build") {
+      steps {
         sh "${yarn()} build"
       }
     }
