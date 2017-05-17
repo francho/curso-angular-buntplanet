f5b78c624613b0110903074c87355064f797f7a8
Francho Joven <lab@francho.org>
2017-05-17 20:49:33 +0200

    Fix structure for detalle-module

--- src/main/comics/detalle/detalle-module.js
+++ src/main/comics/detalle/detalle-module.js
@@ -1,18 +1,16 @@
 import angular from "angular";
 import uiRouter from "angular-ui-router";
 import material from "angular-material";
-import DetalleController from "./detalle-controller"
+import DetalleComponent from "./detalle-component"
 
 export default angular
   .module('curso.comics.detalle', [uiRouter, material])
 
+  .component('detalleComponent', DetalleComponent)
   .config(['$stateProvider', $stateProvider => $stateProvider.state('curso.comics.detalle', {
     url: '/detalle/:id',
-
+    component: 'detalleComponent'
   })])
-  .component({
-    templateUrl: './comics/detalle/detalle.html',
-    controller: DetalleController,
-    controllerAs: '$ctrl'
-  })
+
+
   .name;
