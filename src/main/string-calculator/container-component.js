09dcef56ea3b4bc1491c266501c8ba2be90835a9
ggalmazor <ggalmazor@gmail.com>
2017-05-18 10:50:53 +0200

    Estructura básica de vertical string calculator

---
+++ src/main/string-calculator/container-component.js
@@ -0,0 +1,8 @@
+export default {
+  template: `
+    <div layout="row">
+      <sc-log flex="50"></sc-log>
+      <sc-calculator flex="50"></sc-calculator>
+    </div>
+  `
+}
--- src/main/string-calculator/string-calculator-module.js
+++ src/main/string-calculator/string-calculator-module.js
@@ -2,15 +2,23 @@ import angular from "angular";
 import uiRouter from "angular-ui-router";
 import material from "angular-material";
 
+import scContainerComponent from './container-component'
+import scLogComponent from './log-component';
+import scCalculatorComponent from './calculator-component';
+
 export default angular
     .module('curso.string-calculator', [
       uiRouter,
       material
     ])
 
+    .component('scContainer', scContainerComponent)
+    .component('scLog', scLogComponent)
+    .component('scCalculator', scCalculatorComponent)
+
     .config(['$stateProvider', $stateProvider => $stateProvider.state('curso.string-calculator', {
       url: '/string-calculator',
-      template: '<h1>String calculator!!!</h1>'
+      component: 'scContainer'
     })])
 
     .name;
