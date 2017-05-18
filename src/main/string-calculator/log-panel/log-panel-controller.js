4072fa99f5039e6ce9aa05b54c2d9dcebb3f7b4e
Francho Joven <lab@francho.org>
2017-05-18 11:53:02 +0200

    Log entries scaffold

--- src/main/string-calculator/log-panel/log-panel-controller.js
+++ src/main/string-calculator/log-panel/log-panel-controller.js
@@ -1,6 +1,10 @@
 
 export default class LogPanelController {
   constructor() {
+    this.logEntries = [
+      { operation: '1,4', result: 5},
+      { operation: '1,4,6', result: 11}
+    ]
   }
 
   calculate() {
